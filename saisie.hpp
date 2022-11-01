12c559015336a7305d10a930b2d2072e5e313286
Nicolas Sonnard <114679645+nicosonna1@users.noreply.github.com>
2022-11-01 19:07:56 +0100

    premier commit
    premier commit

---
+++ saisie.hpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------
+// Fichier        : saisie.hpp
+// Version        : 01 - 2022-11-22
+// Auteur(s)      : Sangnakkara Julia et Sonnard Nicolas
+// But            : ce fichier ne contient uniquement les
+//                  déclarations mises à disposition par la librairie
+//
+//                  Ce fichier doit  documenter les elements
+//                  mis à disposition de l'utilisateur
+// Modifications  :
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+//-------------------------------------------------------------------------
+// /!\ eviter les inclusions multiples
+#ifndef saisie
+
+
+#endif
