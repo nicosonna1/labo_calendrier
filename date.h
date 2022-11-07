10703b73496ea240f091b45fade0bfebb123e0aa
jsangnak <julia.sangnakkara@heig-vd.ch>
2022-11-07 11:51:52 +0100

    finale modif des hpp en h - avec fichiers...

---
+++ date.h
@@ -0,0 +1,23 @@
+//---------------------------------------------------------
+// Demo           : date
+// Fichier        : date.cpp
+// Version        : 01 - 2022-11-06
+// Auteur(s)      : Sangnakkara Julia && Sonnard Nicolas
+// But            : Declaration de la libraire date
+//                  Librairie permettant la manipulation des dates
+//
+// Modifications  :
+// Remarque(s)    : NILL
+//---------------------------------------------------------
+
+#ifndef INCLUDES_DATE_H
+#define INCLUDES_DATE_H
+#include <cstdlib>
+
+// prototype des fonctions
+
+bool         estBissextile(const int& annee_calendrier);
+int          nbJoursMois(int& numero_mois, const int& annee_calendrier, int& totalJours);
+unsigned int premier_jour_janvier(const int& annee);
+
+#endif
